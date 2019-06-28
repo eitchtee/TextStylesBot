0817ffc6271047b866ec4b1d03b822100736761d
eitchtee <flaips.2@gmail.com>
2019-06-27 22:53:58 -0300

    Initial commit

---
+++ app/bot.py
@@ -0,0 +1,142 @@
+
+from uuid import uuid4
+
+from telegram.utils.helpers import escape_markdown
+
+from telegram import InlineQueryResultArticle, ParseMode, \
+    InputTextMessageContent
+from telegram.ext import Updater, InlineQueryHandler, CommandHandler
+import logging
+from complex_generators import *
+from config import BOT_TOKEN
+
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - '
+                           '%(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+# Define a few command handlers. These usually take the two arguments bot and
+# update. Error handlers also receive the raised TelegramError object in error.
+# def start(bot, update):
+#     """Send a message when the command /start is issued."""
+#     update.message.reply_text('Hi!')
+#
+#
+# def help(bot, update):
+#     """Send a message when the command /help is issued."""
+#     update.message.reply_text('Help!')
+
+
+def inlinequery(bot, update):
+    """Handle the inline query."""
+    query = update.inline_query.query
+    results = [
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Zalgo",
+            description="Z̙͑͘a̵̺̳̫̅́́͋l̝̠͑̃͘͢ǵ̨͎̰͈͂͆̑ơ̳͚̳͒ W̹͛͝a̛͙̫̤͌ṋ͖̙̇ͧ̊͜"
+                        "t͙ͮ̀̈́ͣ͞ͅsͭ̐ͥ͢͜ Ÿ̶͈́ͣ͋o̡͖̜͓͆̿ų̜͍͎͛͌̏ͨ",
+            input_message_content=InputTextMessageContent(
+                zalgo_txt(query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Up and Down",
+            description="bRoKeN cApSlOcK",
+            input_message_content=InputTextMessageContent(
+                upper_and_lower(query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Cebolinha",
+            description="Troque seu R por um L",
+            input_message_content=InputTextMessageContent(
+                cebolinha(query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Binary",
+            description="0s and 1s",
+            input_message_content=InputTextMessageContent(
+                ' '.join(format(ord(x), 'b') for x in query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Double Struck",
+            description="𝔽𝕒𝕟𝕔𝕪",
+            input_message_content=InputTextMessageContent(
+                double_struck(query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Cursive",
+            description="𝓐𝓵𝓼𝓸 𝓯𝓪𝓷𝓬𝔂",
+            input_message_content=InputTextMessageContent(
+                cursive(query))),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Large",
+            description="ＢＩＧ！",
+            input_message_content=InputTextMessageContent(
+                large(query))),
+
+
+
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Bold",
+            description="*text*",
+            input_message_content=InputTextMessageContent(
+                "*{}*".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Italic",
+            description="_text_",
+            input_message_content=InputTextMessageContent(
+                message_text="_{}_".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+        InlineQueryResultArticle(
+            id=uuid4(),
+            title="Monospace",
+            description="```text```",
+            input_message_content=InputTextMessageContent(
+                message_text="```{}```".format(escape_markdown(query)),
+                parse_mode=ParseMode.MARKDOWN)),
+    ]
+
+    update.inline_query.answer(results)
+
+
+def error(bot, update, erro):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, erro)
+
+
+def main():
+    # Create the Updater and pass it your bot's token.
+    updater = Updater(BOT_TOKEN)
+
+    # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    # dp.add_handler(CommandHandler("start", start))
+    # dp.add_handler(CommandHandler("help", help))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(InlineQueryHandler(inlinequery))
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Block until the user presses Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
---
+++ app/complex_generators.py
@@ -0,0 +1,177 @@
+import random
+
+
+def cebolinha(txt):
+    txt = txt.replace('\n', ' @¨# ')
+    txt = txt.split()
+
+    final_txt = []
+    for word in txt:
+        word.strip()
+        if word == '@¨#':
+            word = word.replace('@¨#', '\n')
+        else:
+            check_word = ''.join([i for i in word if i.isalpha()])
+            if check_word[len(check_word) - 1] != 'r' and check_word[
+                                                    len(check_word) - 1] != 'R':
+                word = word.replace('r', 'l').replace('R', 'L')
+            else:
+                count_r_down = word.count('r')
+                count_r_upper = word.count('R')
+                word = word.replace('r', 'l', count_r_down - 1).\
+                    replace('R',
+                            'L',
+                            count_r_upper - 1)
+
+        final_txt.append(word + ' ')
+
+    final_txt = ''.join(final_txt).replace(' \n ', '\n').replace('\n ',
+                                                                 '\n').replace(
+        ' \n', '\n')
+
+    return final_txt
+
+
+def upper_and_lower(txt):
+    capitalize = 0
+    final_txt = []
+    for letter in txt:
+        if capitalize:
+            letter = letter.upper()
+            capitalize = 0
+        else:
+            letter = letter.lower()
+            capitalize = 1
+
+        final_txt.append(letter)
+
+    return ''.join(final_txt)
+
+
+class Zalgo:
+    def __init__(self):
+        self.numAccentsUp = (1, 3)
+        self.numAccentsDown = (1, 3)
+        self.numAccentsMiddle = (1, 2)
+        self.maxAccentsPerLetter = 3
+        # downward going diacritics
+        self.dd = ['̖', ' ̗', ' ̘', ' ̙', ' ̜', ' ̝', ' ̞', ' ̟', ' ̠', ' ̤',
+                   ' ̥', ' ̦', ' ̩', ' ̪', ' ̫', ' ̬', ' ̭', ' ̮', ' ̯', ' ̰',
+                   ' ̱', ' ̲', ' ̳', ' ̹', ' ̺', ' ̻', ' ̼', ' ͅ', ' ͇', ' ͈',
+                   ' ͉', ' ͍', ' ͎', ' ͓', ' ͔', ' ͕', ' ͖', ' ͙', ' ͚', ' ', ]
+        # upward diacritics
+        self.du = [' ̍', ' ̎', ' ̄', ' ̅', ' ̿', ' ̑', ' ̆', ' ̐', ' ͒', ' ͗',
+                   ' ͑', ' ̇', ' ̈', ' ̊', ' ͂', ' ̓', ' ̈́', ' ͊', ' ͋', ' ͌',
+                   ' ̃', ' ̂', ' ̌', ' ͐', ' ́', ' ̋', ' ̏', ' ̽', ' ̉', ' ͣ',
+                   ' ͤ', ' ͥ', ' ͦ', ' ͧ', ' ͨ', ' ͩ', ' ͪ', ' ͫ', ' ͬ', ' ͭ',
+                   ' ͮ', ' ͯ', ' ̾', ' ͛', ' ͆', ' ̚', ]
+        # middle diacritics
+        self.dm = [' ̕', ' ̛', ' ̀', ' ́', ' ͘', ' ̡', ' ̢', ' ̧', ' ̨', ' ̴',
+                   ' ̵', ' ̶', ' ͜', ' ͝', ' ͞', ' ͟', ' ͠', ' ͢', ' ̸', ' ̷',
+                   ' ͡', ]
+
+    def zalgofy(self, text):
+        # Zalgofy a string
+        # get the letters list
+        letters = list(text)  # ['t','e','s','t',' ','t',...]
+        # print(letters)
+        # new_word = ''
+        new_letters = []
+
+        # for each letter, add some diacritics of all varieties
+        for letter in letters:  # 'p', etc...
+            a = letter  # create a dummy letter
+
+            # skip this letter we can't add a diacritic to it
+            if not a.isalpha():
+                new_letters.append(a)
+                continue
+
+            num_accents = 0
+            num_u = random.randint(self.numAccentsUp[0], self.numAccentsUp[1])
+            num_d = random.randint(self.numAccentsDown[0],
+                                   self.numAccentsDown[1])
+            num_m = random.randint(self.numAccentsMiddle[0],
+                                   self.numAccentsMiddle[1])
+            # Try to add accents to the letter, will add an upper, lower,
+            # or middle accent randomly until
+            # either num_accents == self.maxAccentsPerLetter or we have added
+            # the maximum upper, middle and lower accents. Denoted
+            # by num_u, num_d, and num_m
+            while num_accents < self.maxAccentsPerLetter and num_u + num_m + \
+                    num_d != 0:
+                randint = random.randint(0,
+                                         2)  # randomly choose what accent
+                # type to add
+                if randint == 0:
+                    if num_u > 0:
+                        a = self.combine_with_diacritic(a, self.du)
+                        num_accents += 1
+                        num_u -= 1
+                elif randint == 1:
+                    if num_d > 0:
+                        a = self.combine_with_diacritic(a, self.dd)
+                        num_d -= 1
+                        num_accents += 1
+                else:
+                    if num_m > 0:
+                        a = self.combine_with_diacritic(a, self.dm)
+                        num_m -= 1
+                        num_accents += 1
+
+            # a = a.replace(" ","") #remove any spaces, this also gives it
+            # the zalgo text look
+            # print('accented a letter: ' + a)
+            new_letters.append(a)
+
+        new_word = ''.join(new_letters)
+        return new_word
+
+    @staticmethod
+    def combine_with_diacritic(letter, diacritic_list):
+        # Combines letter and a random character from diacriticLis
+        return letter.strip() + diacritic_list[
+            random.randrange(0, len(diacritic_list))].strip()
+
+
+def zalgo_txt(txt):
+    z = Zalgo()
+    z.numAccentsUp = (1, 3)
+    z.numAccentsDown = (1, 3)
+    z.numAccentsMiddle = (1, 2)
+    z.maxAccentsPerLetter = 40
+
+    return z.zalgofy(txt)
+
+
+def double_struck(txt):
+    normal_letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    double_struck_letter = "𝟘𝟙𝟚𝟛𝟜𝟝𝟞𝟟𝟠𝟡𝕒𝕓𝕔𝕕𝕖𝕗𝕘𝕙𝕚𝕛𝕜𝕝𝕞𝕟𝕠𝕡𝕢𝕣𝕤𝕥𝕦𝕧𝕨𝕩𝕪𝕫𝔸𝔹ℂ𝔻𝔼𝔽𝔾ℍ𝕀𝕁𝕂𝕃𝕄ℕ𝕆ℙℚℝ𝕊𝕋𝕌𝕍𝕎𝕏𝕐ℤ"
+
+    trantab = txt.maketrans(normal_letters, double_struck_letter)
+
+    txt = txt.translate(trantab)
+    return txt
+
+
+def cursive(txt):
+    normal_letters = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    cursive_letter = "0123456789𝓪𝓫𝓬𝓭𝓮𝓯𝓰𝓱𝓲𝓳𝓴𝓵𝓶𝓷𝓸𝓹𝓺𝓻𝓼𝓽𝓾𝓿𝔀𝔁𝔂𝔃𝓐𝓑𝓒𝓓𝓔𝓕𝓖𝓗𝓘𝓙𝓚𝓛𝓜𝓝𝓞𝓟𝓠𝓡𝓢𝓣𝓤𝓥𝓦𝓧𝓨𝓩"
+
+    trantab = txt.maketrans(normal_letters, cursive_letter)
+
+    txt = txt.translate(trantab)
+    return txt
+
+
+def large(txt):
+    normal_letters = "`1234567890-=~!@#$%^&*()_+qwertyuiop[]QWERTYUIOP{}|asdfghjkl;'ASDFGHJKL:\zxcvbnm,./ZXCVBNM<>?"
+    fancy_letter = "`１２３４５６７８９０－＝~！＠＃＄％^＆＊（）_＋ｑｗｅｒｔｙｕｉｏｐ[]ＱＷＥＲＴＹＵＩＯＰ{}|ａｓｄｆｇｈｊｋｌ；＇ＡＳＤＦＧＨＪＫＬ：\ｚｘｃｖｂｎｍ，．／ＺＸＣＶＢＮＭ<>？"
+
+    trantab = txt.maketrans(normal_letters, fancy_letter)
+
+    txt = txt.translate(trantab)
+    return txt
+
+
+print(large('BIG!'))
